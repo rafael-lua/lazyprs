2405d09f59a7c6c1d6438111285c01aac5f5266f
rafael-lua <rafael.tragueta@lemon.energy>
2025-04-01 13:46:28 -0300

    feat: create cli command logic

---
+++ index.ts
@@ -0,0 +1,130 @@
+#!/usr/bin/env node
+
+import { defineCommand, runMain } from "citty"
+import { consola } from "consola"
+import { execa, execaSync } from "execa"
+import { nanoid } from "nanoid"
+
+const cli = defineCommand({
+  meta: {
+    name: "lazyprs",
+    version: "1.0.0",
+    description: "Make quick PRs from a range of commits",
+  },
+  args: {
+    base: {
+      type: "positional",
+      description: "Branch to open pr against",
+      required: true,
+    },
+    branch: {
+      type: "string",
+      description: "Created branch name",
+    },
+    title: {
+      type: "string",
+      description: "Title to use for pr",
+    },
+    from: {
+      type: "string",
+      description:
+        "Branch or sha to use in merge-base for commit range. Default is {base}",
+    },
+    draft: {
+      type: "boolean",
+      description: "Open as draft",
+    },
+    keep: {
+      type: "boolean",
+      description: "Keep local branch after pr is created",
+    },
+    ["ignore-templates"]: {
+      type: "boolean",
+      description: "Keep local branch after pr is created",
+    },
+    body: {
+      type: "string",
+      description: "Body to use for pr",
+    },
+  },
+  async run({ args }) {
+    consola.box("Lazy PRs")
+
+    const base = args.base
+    const draft = args.draft
+    const keep = args.keep
+    const ignoreTemplate = args["ignore-templates"]
+    const from = args.from || base
+    const branchName = args.branch || `lazy-prs-${nanoid()}`
+    const title = args.title || `Lazy PR | ${branchName}`
+    const body = args.body || "Automatically created with lazy prs."
+
+    const { stdout: mergeBase } = await execa("git", [
+      "merge-base",
+      "HEAD",
+      from,
+    ])
+
+    const { stdout: logs } = await execa("git", [
+      "log",
+      '--pretty=format:"%h\t%s"',
+      `${mergeBase}..HEAD`,
+    ])
+
+    const logsOptions = logs.split("\n")
+
+    const res = await consola.prompt(
+      "Select the commits to include in the PR",
+      {
+        type: "multiselect",
+        options: logsOptions.map((it) => it.replaceAll('"', "")),
+      }
+    )
+
+    const logsSplitOptions = res.map((it) => it.split("\t"))
+
+    /*
+        - Create a branch with {branchName} from {from}
+        - Cherry pick the selected commits
+        - Push to remote
+        - Open a PR for the created branch against {base} using provided {title}
+    */
+    consola.info(`Creating branch ${branchName} from ${from}`)
+    await execa("git", ["checkout", "-b", branchName, from])
+
+    consola.info(`Cherry picking commits`)
+    logsSplitOptions.forEach((it) => {
+      const [sha, message] = it
+      consola.log(` - commiting ${sha} - ${message}`)
+
+      if (sha)
+        execaSync("git", [
+          "cherry-pick",
+          sha,
+          "--allow-empty",
+          "--keep-redundant-commits",
+        ])
+    })
+
+    consola.info(`Pushing to remote`)
+    await execa("git", ["push", "-u", "origin", "HEAD"])
+
+    consola.info(`Creating PR for ${branchName} against ${base}`)
+    await execa("gh", [
+      "pr",
+      "create",
+      "--base",
+      base,
+      "--title",
+      title,
+      ...(ignoreTemplate ? ["--body", body] : []),
+      ...(draft ? ["--draft"] : []),
+    ])
+
+    await execa("git", ["checkout", "-"])
+    if (!keep) await execa("git", ["branch", "-D", branchName])
+    consola.info("Done!")
+  },
+})
+
+runMain(cli)
